93e0d04faa6c7f2467cac5e4daaac50d8fae10c2
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-07-18 14:05:49 +1100

    Maximum Multiple

---
+++ maxMult.js
@@ -0,0 +1,10 @@
+function maxMultiple(divisor, bound){
+  const mySet = new Set();
+  for(let n=1; n <= bound;n++){
+     if(n % divisor === 0 ){
+    mySet.add(n);
+  }
+  }
+  return Math.max(...mySet);
+}
+console.log(maxMultiple(2,100));
