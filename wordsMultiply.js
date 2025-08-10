6f5596381688f8ff894447fd1ffb5961a5bdef09
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-10 13:55:39 +1100

    Deleted the consolelog

--- wordsMultiply.js
+++ wordsMultiply.js
@@ -3,4 +3,3 @@ function modifyMultiply(str, loc, num) {
   const word = words[loc];            
   return Array(num).fill(word).join('-'); 
 }
-console.log(modifyMultiply("Hello World I am glad we met at this time", 3, 5));
