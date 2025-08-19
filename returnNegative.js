811a7872c25a38f8af252be19aabfbea2ea18790
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-19 14:32:16 +1100

    Refactored using the ternary operator

--- returnNegative.js
+++ returnNegative.js
@@ -1,17 +1,5 @@
 function makeNegative(num) {
-  if (typeof num === 'number'){
-    if (num === 0){
-    return 0
-  }
-  else if(num < 0){
-    return num
-  }
-  else {
-    return -num
-  }
-  }
-  else{
-    return "Not a number"
-  }
+  
+  return (num < 0)? num:-num
   
 }
