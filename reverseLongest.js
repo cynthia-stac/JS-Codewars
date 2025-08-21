c218229b3d51d867eeedb27fa9d188cc3d2ed1d7
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-21 16:05:10 +1100

    Refactored

--- reverseLongest.js
+++ reverseLongest.js
@@ -1,14 +1,3 @@
 function spinWords(string){
-  const words = string.split(' ')
-  const sentence = []
-  for (let word of words){
-    if (word.length >= 5){
-      const reversedWord = word.split('').reverse().join('')
-      sentence.push(reversedWord)
-    }
-    else{
-      sentence.push(word)
-    }
-  }
-  return sentence.join(' ')
+ return string.split(' ').map(word => ((word.length >=5)? word.split('').reverse().join(''):word )).join(' ')
 }
