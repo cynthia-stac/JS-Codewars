c1c1369e363a23ad767e4a8c9d5a62d2d929a426
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-27 17:12:43 +1100

    Tested the count hash map

--- returnOdd.js
+++ returnOdd.js
@@ -5,11 +5,11 @@ for (let num of A ){
 count[num] = (count[num] || 0) +1
  }
 
-// for (let num in count ){
-// if(count[num] % 2 !==0){
-//   return Number(num) ;
-// }
-// }
+for (let num in count ){
+if(count[num] % 2 !==0){
+  return Number(num) ;
+}
+}
 
 return count
 }
