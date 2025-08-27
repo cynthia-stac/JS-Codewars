9ce2508e78e8a81950397d1931b119fac3a4ec1a
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-27 17:11:45 +1100

    Tested the count hash map

--- returnOdd.js
+++ returnOdd.js
@@ -5,9 +5,13 @@ for (let num of A ){
 count[num] = (count[num] || 0) +1
  }
 
-for (let num in count ){
-if(count[num] % 2 !==0){
-  return Number(num) ;
+// for (let num in count ){
+// if(count[num] % 2 !==0){
+//   return Number(num) ;
+// }
+// }
+
+return count
 }
-}
-}
+
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
