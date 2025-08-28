7838633f3b8e4e483a6b9903d67c33e7b2338d49
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-28 16:43:37 +1100

    Refactored

--- digitalRoot.js
+++ digitalRoot.js
@@ -1,10 +1,16 @@
-function digitalRoot(n) {
- const result = n.toString().split('')
- if (result.length === 1){
-   return n
- }
-  else {
-    let sum = result.reduce((acc, val) => acc + Number(val), 0)
-    return digitalRoot(sum)
-  }
+// function digitalRoot(n) {
+//  const result = n.toString().split('')
+//  if (result.length === 1){
+//    return n
+//  }
+//   else {
+//     let sum = result.reduce((acc, val) => acc + Number(val), 0)
+//     return digitalRoot(sum)
+//   }
+// }
+
+function digitalRoot(n){
+    const num = n.toString().split('')
+    return (num.length === 1) ? n : digitalRoot(num.reduce((acc, k) => acc + Number(k), 0))
 }
+
