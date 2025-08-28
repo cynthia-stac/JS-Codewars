b7703d8efb63814a96610c360d32bdae71615191
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-28 15:00:06 +1100

    Refactored the multiples of 3 or 5

--- mut3Or5.js
+++ mut3Or5.js
@@ -1,10 +1,18 @@
+// Solution 1
+
+// function solution(number){
+//    let result = 0
+//   if (number < 0) return 0
+//   for (let i = 0 ; i < number ; i++){
+//     if(i % 3 === 0 || i % 5 === 0){
+//       result += i
+//     }
+//   }
+//   return result
+// }
+
+// Solution 2
+
 function solution(number){
-   let result = 0
-  if (number < 0) return 0
-  for (let i = 0 ; i < number ; i++){
-    if(i % 3 === 0 || i % 5 === 0){
-      result += i
-    }
-  }
-  return result
+return (number < 0)? 0 : [...Array(number).keys()].filter(n => n%3 === 0 || n%5 === 0).reduce((n, el) => n + el , 0)
 }
