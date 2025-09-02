94552902afe35ccaec4a3250f7dbec1f838e9185
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-02 14:58:06 +1100

    Refactored

--- phoneNumber.js
+++ phoneNumber.js
@@ -1,3 +1,18 @@
+// function createPhoneNumber(numbers){
+//   return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`
+// }
+
 function createPhoneNumber(numbers){
-  return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`
+  let helper = '(xxx) xxx-xxxx'
+  
+//   for (let i=0; i < numbers.length; i++){
+//     helper = helper.replace('x', numbers[i])
+//   }
+  
+  for(number of numbers){
+    helper = helper.replace('x', number)
+  }
+  
+  
+  return helper
 }
