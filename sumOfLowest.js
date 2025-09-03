26273b9c34c9360c057a08d0db6935754b611a69
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-03 14:55:28 +1100

    Refactored

--- sumOfLowest.js
+++ sumOfLowest.js
@@ -1,9 +1,14 @@
-function sumTwoSmallestNumbers(numbers) {  
-const sorted = numbers.sort((a,b) => a-b)
-let sliced = sorted.slice(0, 2)
-let sum = 0
-for(let i=0; i<sliced.length; i++){
-    sum = sum + sliced[i]
-}
-return sum
+// function sumTwoSmallestNumbers(numbers) {  
+// const sorted = numbers.sort((a,b) => a-b)
+// let sliced = sorted.slice(0, 2)
+// let sum = 0
+// for(let i=0; i<sliced.length; i++){
+//     sum = sum + sliced[i]
+// }
+// return sum
+// }
+
+function sumTwoSmallestNumbers(numbers){
+    const sorted = numbers.sort((a,b) => a-b)
+    return sorted[0] + sorted [1]
 }
