8ac3744d218728d72b0ebf4b0a09eafb60cfd9e7
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-05 14:36:53 +1100

    Refactored

--- sortedArrays.js
+++ sortedArrays.js
@@ -1,9 +1,15 @@
+// function mergeArrays(a, b) {
+  
+//   const firstSort = a.sort((x, y) => x - y)
+//   const secondSort = b.sort((x, y) => x - y)
+//   const merged = a.concat(b)
+//   const sorted = merged.sort((x, y) => x - y)
+  
+//   return [...new Set(sorted)]
+// }
+
 function mergeArrays(a, b) {
   
-  const firstSort = a.sort((x, y) => x - y)
-  const secondSort = b.sort((x, y) => x - y)
-  const merged = a.concat(b)
-  const sorted = merged.sort((x, y) => x - y)
+  return [...new Set(a.concat(b))].sort((a, b) => a - b)
   
-  return [...new Set(sorted)]
 }
