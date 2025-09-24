ecaddb60260d782ab17b886b68a6458088e4990c
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-24 16:16:57 +0200

    Replace all letters with their positions

---
+++ numberLetter.js
@@ -0,0 +1,117 @@
+function alphabetPosition(text) {
+  
+  let r = ""
+  for (let txt of text){
+    txt = txt.toLowerCase()
+  switch(txt){
+      
+      case 'a':
+      r += '1 '
+      break;
+      
+      case 'b':
+      r += '2 '
+      break;
+      
+      case 'c':
+      r += '3 '
+      break;
+      
+      case 'd':
+      r += '4 '
+      break;
+      
+      case 'e':
+      r += '5 '
+      break;
+      
+      case 'f':
+      r += '6 '
+      break;
+      
+      case 'g':
+      r += '7 '
+      break;
+      
+      case 'h':
+      r += '8 '
+      break;
+      
+      case 'i':
+      r += '9 '
+      break;
+      
+      case 'j':
+      r += '10 '
+      break;
+      
+      case 'k':
+      r += '11 '
+      break;
+      
+      case 'l':
+      r += '12 '
+      break;
+      
+      case 'm':
+      r += '13 '
+      break;
+      
+      case 'n':
+      r += '14 '
+      break;
+      
+      case 'o':
+      r += '15 '
+      break;
+      
+      case 'p':
+      r += '16 '
+      break;
+      
+      case 'q':
+      r += '17 '
+      break;
+      
+      case 'r':
+      r += '18 '
+      break;
+      
+      case 's':
+      r += '19 '
+      break;
+      
+      case 't':
+      r += '20 '
+      break;
+      
+      case 'u':
+      r += '21 '
+      break;
+      
+      case 'v':
+      r += '22 '
+      break;
+      
+      case 'w':
+      r += '23 '
+      break;
+      
+      case 'x':
+      r += '24 '
+      break;
+      
+      
+      case 'y':
+      r += '25 '
+      break;
+      
+      case 'z':
+      r += '26 '
+     
+    
+      
+  }
+  }
+  return r.trim();
+}
