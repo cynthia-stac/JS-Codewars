bf5c84a4dafed443c5609cdc88054ae83cfd588d
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-24 14:43:36 +0200

    Swap the head and the tail

---
+++ headTail.js
@@ -0,0 +1,8 @@
+function swapHeadAndTail(arr) {
+ let mid = Math.floor(arr.length / 2)
+ let head = arr.slice(0, mid)
+ let tail = arr.slice(-mid)
+ let half = arr[mid]
+ 
+ return (arr.length % 2 === 0) ? [...tail, ...head] : [...tail, half, ...head]
+}
