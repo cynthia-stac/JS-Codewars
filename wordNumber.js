b8b13cf5b7a512a74c8c41b1d4bcac5fb9cea6d3
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-25 19:03:51 +0200

    Refactored

--- wordNumber.js
+++ wordNumber.js
@@ -1,5 +1,5 @@
 function order(text){
-    if (text === " ") return text
+    if (text === "") return ""
   let words = text.split(' ')
   return words.sort((a, b) => a.split('').filter(v => !isNaN(parseInt(v))) - b.split('').filter(el => !isNaN(parseInt(el)))).join(' ')
 }
