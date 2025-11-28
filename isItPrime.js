b5d6587f8b62ba1bd6a1265f23c8ed017783bf9d
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-11-28 12:41:31 +0200

    Check if this number is prime according to the limit

---
+++ isItPrime.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
+
+  let limit = Math.sqrt(num);
+
+  for (let i = 3; i <= limit; i += 2) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
