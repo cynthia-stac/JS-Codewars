77ec3340ad752d7e7e6aa7c3e9812b1f409c70c6
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-12-09 11:38:26 +0200

    Refactor

--- hexToRGB.js
+++ hexToRGB.js
@@ -1,10 +1,10 @@
-function hexStringToRGB(hexString) {
-  let hex = hexString.toUpperCase()
-  hex = hexString.replace('#','')
+// function hexStringToRGB(hexString) {
+//   let hex = hexString.toUpperCase()
+//   hex = hexString.replace('#','')
   
-  let r = parseInt(hex.slice(0,2) ,16)
-  let g = parseInt(hex.slice(2,4) ,16)
-  let b = parseInt(hex.slice(4,6) ,16)
+//   let r = parseInt(hex.slice(0,2) ,16)
+//   let g = parseInt(hex.slice(2,4) ,16)
+//   let b = parseInt(hex.slice(4,6) ,16)
   
-  return {r ,g ,b}
-}
+//   return {r ,g ,b}
+// }
