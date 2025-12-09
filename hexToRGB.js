e67f2c89ff2ef26886d341f5dfa4db0dec6f2614
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-12-09 11:39:51 +0200

    Refactor in one line

--- hexToRGB.js
+++ hexToRGB.js
@@ -8,3 +8,13 @@
   
 //   return {r ,g ,b}
 // }
+
+//Refactoring
+
+function hexStringToRGB(hexString) {
+
+  return {r : parseInt(hexString.slice(1,3) ,16),
+          g : parseInt(hexString.slice(3,5) ,16),
+          b : parseInt(hexString.slice(5) ,16)
+         }
+}
