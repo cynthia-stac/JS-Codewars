0446ab55f3e54aeb31b5fe21dfedcc2cac52911b
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-01-26 14:31:18 +0200

    Written in both javascript and typescript

--- noHighestLowest.js
+++ noHighestLowest.js
@@ -1,9 +1,9 @@
-// function sumArray(array) {
+function sumArray(array) {
    
-//  if (!array || array.length < 2) return 0 
-//   return array.reduce( (sum, v) => sum + v, 0) - Math.max(...array) - Math.min(...array)
+ if (!array || array.length < 2) return 0 
+  return array.reduce( (sum, v) => sum + v, 0) - Math.max(...array) - Math.min(...array)
   
-// }
+}
 
 export function sumArray(array:number[] | null) : number {
   if (!array || array.length < 2) return 0 
