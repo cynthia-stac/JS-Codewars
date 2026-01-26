81a9f78f718c1d37b03c05429aab4c1445b5efa5
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-01-26 14:30:26 +0200

    Refactored

--- noHighestLowest.js
+++ noHighestLowest.js
@@ -1,6 +1,12 @@
-function sumArray(array) {
+// function sumArray(array) {
    
- if (!array || array.length < 2) return 0 
+//  if (!array || array.length < 2) return 0 
+//   return array.reduce( (sum, v) => sum + v, 0) - Math.max(...array) - Math.min(...array)
+  
+// }
+
+export function sumArray(array:number[] | null) : number {
+  if (!array || array.length < 2) return 0 
   return array.reduce( (sum, v) => sum + v, 0) - Math.max(...array) - Math.min(...array)
   
 }
