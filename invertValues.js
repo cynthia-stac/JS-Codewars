1d9f8c0fb7893b7c104a339d889c52e95469a046
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-01-27 14:41:34 +0200

    Comment typescript to remove errors

--- invertValues.js
+++ invertValues.js
@@ -2,7 +2,10 @@ function invert(array) {
    return array.map( v => -v)
 }
 
+// Typescript
 
-export function invert(array: number[]): number[] {
-   return array.map( v => -v)
-}
+
+
+// export function invert(array: number[]): number[] {
+//    return array.map( v => -v)
+// }
