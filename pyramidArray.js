123a8cfbf017996487ba39c322450d9003980ee6
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-05 17:55:58 +0200

    Refactor in typescript langauge

--- pyramidArray.js
+++ pyramidArray.js
@@ -6,3 +6,14 @@ function pyramid(n) {
     }
     return arr.map(v => v.map(s => Number(s)))
 }
+
+// Typescript
+
+// export function pyramid(n: number) : Array<Array<Number>> {
+//     let arr = []
+//     if(n === 0) return []
+//     for(let i = 1; i <= n; i++){
+//         arr.push([...'1'.repeat(i)])
+//     }
+//     return arr.map(v => v.map(s => Number(s)))
+// }
