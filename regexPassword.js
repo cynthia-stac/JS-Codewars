6500cf1c58d33226ee4f4a994ad969bd2962de80
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-11 11:58:12 +0200

    Refactor

--- regexPassword.js
+++ regexPassword.js
@@ -1,2 +1,7 @@
 // assign your RegExp to REGEXP:
 const REGEXP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{6,}$/;
+
+// Typescript
+
+// // assign your RegExp to REGEXP
+// export const REGEXP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{6,}$/;
