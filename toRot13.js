083b16a5136017ee38e534268624423e65199f3d
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-12 14:39:04 +0200

    Refactor to typescript

--- toRot13.js
+++ toRot13.js
@@ -15,3 +15,23 @@ function rot13(str) {
 
   return result
 }
+
+// Typescript
+
+// export function rot13(str: string): string {
+//   const normal = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//   const rotated = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+
+//   let result = ""
+
+//   for (let char of str) {
+//     if (normal.includes(char)) {
+//       let index = normal.indexOf(char)
+//       result += rotated[index]
+//     } else {
+//       result += char
+//     }
+//   }
+
+//   return result
+// }
