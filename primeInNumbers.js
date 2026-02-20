cc787d7f41c071e72e462faf3e2d95883f5de9b4
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-20 16:23:08 +0200

    Refactor the prime numbers in typescript

--- primeInNumbers.js
+++ primeInNumbers.js
@@ -25,3 +25,34 @@ function primeFactors(n) {
 
   return result
 }
+
+
+// Typescript
+
+// export const primeFactors = (n:number): string => {
+//    let result = ""
+//   let divisor = 2
+
+//   while (n > 1) {
+//     let count = 0
+
+    
+//     while (n % divisor === 0) {
+//       n = n / divisor
+//       count++
+//     }
+
+  
+//     if (count > 0) {
+//       if (count === 1) {
+//         result += "(" + divisor + ")"
+//       } else {
+//         result += "(" + divisor + "**" + count + ")"
+//       }
+//     }
+
+//     divisor++
+//   }
+
+//   return result
+// }
