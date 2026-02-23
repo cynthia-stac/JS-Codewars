eaa4c5c675f25b81a376b7bbce8b430026e6d331
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-23 14:48:41 +0200

    Refactor the codewar to typescript

--- missingLetter.js
+++ missingLetter.js
@@ -6,3 +6,16 @@ function findMissingLetter(array) {
      upperCase.slice(upperCase.indexOf(array[0]), upperCase.indexOf(array[array.length - 1] )+ 1)
     return tempArr.filter(v => ! array.includes(v)).join('')
 }
+
+
+// Typescript
+
+// export function findMissingLetter(array:string[]):string
+// {
+//       const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+//     const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//     const [...tempArr] = array[0] >= 'a' && array[0] <= 'z' ?
+//      lowerCase.slice(lowerCase.indexOf(array[0]), lowerCase.indexOf(array[array.length - 1] )+ 1) :
+//      upperCase.slice(upperCase.indexOf(array[0]), upperCase.indexOf(array[array.length - 1] )+ 1)
+//     return tempArr.filter(v => ! array.includes(v)).join('')
+// }
