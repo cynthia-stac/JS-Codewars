bbf4e382eba2d34deb281e0c0c63ac8e31b6a027
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-02-23 14:47:21 +0200

    Return the missing letter of the given sequence

---
+++ missingLetter.js
@@ -0,0 +1,8 @@
+function findMissingLetter(array) {
+    const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+    const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const [...tempArr] = array[0] >= 'a' && array[0] <= 'z' ?
+     lowerCase.slice(lowerCase.indexOf(array[0]), lowerCase.indexOf(array[array.length - 1] )+ 1) :
+     upperCase.slice(upperCase.indexOf(array[0]), upperCase.indexOf(array[array.length - 1] )+ 1)
+    return tempArr.filter(v => ! array.includes(v)).join('')
+}
