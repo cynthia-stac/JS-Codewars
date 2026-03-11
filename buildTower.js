0265b43e8919127cd5719c89e51b14130daaf49b
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-11 20:15:38 +0200

    Comment the typescript to remove errors

--- buildTower.js
+++ buildTower.js
@@ -15,21 +15,21 @@ function towerBuilder(nFloors) {
 }
 
 
-Typescript
+// Typescript
 
 
-export const towerBuilder = (nFloors: number): string[] => {
-    let tower = []
+// export const towerBuilder = (nFloors: number): string[] => {
+//     let tower = []
   
-  for(let i = 1; i <= nFloors; i++){
-    let spaces = nFloors - i
-    let stars = 2 * i - 1
+//   for(let i = 1; i <= nFloors; i++){
+//     let spaces = nFloors - i
+//     let stars = 2 * i - 1
     
-    let row = ' '.repeat(spaces) + '*'.repeat(stars) + ' '.repeat(spaces)
+//     let row = ' '.repeat(spaces) + '*'.repeat(stars) + ' '.repeat(spaces)
     
-    tower.push(row)
-  }
+//     tower.push(row)
+//   }
   
   
-  return tower
-}
+//   return tower
+// }
