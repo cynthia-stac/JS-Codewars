86c939551c1cdda668f8bebe812f5647fe3d1d77
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-11 20:14:56 +0200

    Refactor with typescript

--- buildTower.js
+++ buildTower.js
@@ -16,3 +16,20 @@ function towerBuilder(nFloors) {
 
 
 Typescript
+
+
+export const towerBuilder = (nFloors: number): string[] => {
+    let tower = []
+  
+  for(let i = 1; i <= nFloors; i++){
+    let spaces = nFloors - i
+    let stars = 2 * i - 1
+    
+    let row = ' '.repeat(spaces) + '*'.repeat(stars) + ' '.repeat(spaces)
+    
+    tower.push(row)
+  }
+  
+  
+  return tower
+}
