6c66b14c263a472c554e0b29962e7b69389b60d7
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-11 20:06:17 +0200

    Build a tower using a sequence of stars

---
+++ buildTower.js
@@ -0,0 +1,15 @@
+function towerBuilder(nFloors) {
+  let tower = []
+  
+  for(let i = 1; i <= nFloors; i++){
+    let spaces = nFloors - i
+    let stars = 2 * i - 1
+    
+    let row = ' '.repeat(spaces) + '*'.repeat(stars) + ' '.repeat(spaces)
+    
+    tower.push(row)
+  }
+  
+  
+  return tower
+}
