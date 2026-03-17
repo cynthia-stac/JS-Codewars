d0f62cecfddf6161eeac3c9a6c4279d127818718
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-17 18:42:45 +0200

    Refactor the paginationHelper using typescript

--- paginationHelper.js
+++ paginationHelper.js
@@ -40,3 +40,57 @@ class PaginationHelper {
 
 Typescript
 
+export class PaginationHelper {
+  private collection: unknown[];
+  private itemsPerPage: number;
+
+  public constructor(collection: unknown[], itemsPerPage: number) {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
+  }
+
+ 
+  public itemCount(): number {
+    return this.collection.length;
+  }
+
+  
+  public pageCount(): number {
+    return Math.ceil(this.collection.length / this.itemsPerPage);
+  }
+
+  
+  public pageItemCount(pageIndex: number): number {
+    const totalPages = this.pageCount();
+
+    
+    if (pageIndex < 0 || pageIndex >= totalPages) {
+      return -1;
+    }
+
+   
+    if (pageIndex === totalPages - 1) {
+      const leftover = this.collection.length % this.itemsPerPage;
+
+      
+      if (leftover === 0) {
+        return this.itemsPerPage;
+      }
+
+      return leftover;
+    }
+
+  
+    return this.itemsPerPage;
+  }
+
+  
+  public pageIndex(itemIndex: number): number {
+    
+    if (itemIndex < 0 || itemIndex >= this.collection.length) {
+      return -1;
+    }
+
+    return Math.floor(itemIndex / this.itemsPerPage);
+  }
+}
