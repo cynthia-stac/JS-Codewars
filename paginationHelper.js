4c16a28ca81ca340e55b08eeaa2d0f9e6c801a02
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-17 18:36:40 +0200

    Help in the creation of pagination

---
+++ paginationHelper.js
@@ -0,0 +1,39 @@
+class PaginationHelper {
+	constructor(collection, itemsPerPage) {
+	  this.collection = collection
+    this.itemsPerPage = itemsPerPage
+	}
+	itemCount() {
+	  return this.collection.length
+	}
+	pageCount() {
+    return Math.ceil(this.collection.length / this.itemsPerPage)
+	}
+	pageItemCount(pageIndex) {
+  let totalPages = this.pageCount()
+  
+  if(pageIndex < 0 || pageIndex >= totalPages){
+    return -1
+  }
+  if (pageIndex === totalPages - 1) {
+    let leftoverItems = this.collection.length % this.itemsPerPage
+
+    if (leftoverItems === 0) {
+      return this.itemsPerPage
+    }
+
+    return leftoverItems
+  }
+
+  return this.itemsPerPage
+
+	}
+	pageIndex(itemIndex) {
+	if(itemIndex < 0 || itemIndex >= this.collection.length){
+    return -1
+  }
+  let pageNumber = Math.floor(itemIndex / this.itemsPerPage)
+  
+  return pageNumber
+	}
+}
