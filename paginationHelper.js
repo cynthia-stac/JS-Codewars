9d94aa5a8b7a345a1fae01c353de2b32734e8a01
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-17 18:43:24 +0200

    Comment the typescript to remove the errors

--- paginationHelper.js
+++ paginationHelper.js
@@ -38,59 +38,59 @@ class PaginationHelper {
 	}
 }
 
-Typescript
+// Typescript
 
-export class PaginationHelper {
-  private collection: unknown[];
-  private itemsPerPage: number;
+// export class PaginationHelper {
+//   private collection: unknown[];
+//   private itemsPerPage: number;
 
-  public constructor(collection: unknown[], itemsPerPage: number) {
-    this.collection = collection;
-    this.itemsPerPage = itemsPerPage;
-  }
+//   public constructor(collection: unknown[], itemsPerPage: number) {
+//     this.collection = collection;
+//     this.itemsPerPage = itemsPerPage;
+//   }
 
  
-  public itemCount(): number {
-    return this.collection.length;
-  }
+//   public itemCount(): number {
+//     return this.collection.length;
+//   }
 
   
-  public pageCount(): number {
-    return Math.ceil(this.collection.length / this.itemsPerPage);
-  }
+//   public pageCount(): number {
+//     return Math.ceil(this.collection.length / this.itemsPerPage);
+//   }
 
   
-  public pageItemCount(pageIndex: number): number {
-    const totalPages = this.pageCount();
+//   public pageItemCount(pageIndex: number): number {
+//     const totalPages = this.pageCount();
 
     
-    if (pageIndex < 0 || pageIndex >= totalPages) {
-      return -1;
-    }
+//     if (pageIndex < 0 || pageIndex >= totalPages) {
+//       return -1;
+//     }
 
    
-    if (pageIndex === totalPages - 1) {
-      const leftover = this.collection.length % this.itemsPerPage;
+//     if (pageIndex === totalPages - 1) {
+//       const leftover = this.collection.length % this.itemsPerPage;
 
       
-      if (leftover === 0) {
-        return this.itemsPerPage;
-      }
+//       if (leftover === 0) {
+//         return this.itemsPerPage;
+//       }
 
-      return leftover;
-    }
-
-  
-    return this.itemsPerPage;
-  }
+//       return leftover;
+//     }
 
   
-  public pageIndex(itemIndex: number): number {
+//     return this.itemsPerPage;
+//   }
+
+  
+//   public pageIndex(itemIndex: number): number {
     
-    if (itemIndex < 0 || itemIndex >= this.collection.length) {
-      return -1;
-    }
+//     if (itemIndex < 0 || itemIndex >= this.collection.length) {
+//       return -1;
+//     }
 
-    return Math.floor(itemIndex / this.itemsPerPage);
-  }
-}
+//     return Math.floor(itemIndex / this.itemsPerPage);
+//   }
+// }
