6f70ce9c29e766238ab2296c68eb23560dade036
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-20 15:19:51 +0200

    Count these IP adresses

---
+++ countIps.js
@@ -0,0 +1,16 @@
+function sumPairs(ints, s) {
+  let seen = new Set();
+
+  for (let i = 0; i < ints.length; i++) {
+    let current = ints[i];
+    let needed = s - current;
+
+    if (seen.has(needed)) {
+      return [needed, current];
+    }
+
+    seen.add(current);
+  }
+
+  return undefined;
+}
