ce9a7509c28193633e4733c85205fe98dd20cb33
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-03-20 15:24:24 +0200

    Refactor with typescript

--- sumPairs.js
+++ sumPairs.js
@@ -18,3 +18,19 @@ function sumPairs(ints, s) {
 
 Typescript
 
+export function sumPairs(ints: number[], s: number): [number, number] | void {  
+    let seen = new Set();
+
+  for (let i = 0; i < ints.length; i++) {
+    let current = ints[i];
+    let needed = s - current;
+
+    if (seen.has(needed)) {
+      return [needed, current];
+    }
+
+    seen.add(current);
+  }
+
+  return undefined;
+}
